b963a3d50a224b86942c0827a396c12fe3a6240b
Keren Ye <yekeren.cn@gmail.com>
2015-11-29 17:27:48 -0500

    files

--- tools/upload.sh
+++ tools/upload.sh
@@ -4,7 +4,7 @@ i=0
 ls -l webdata | awk '{print $9}'| while read line; do
     curl -d "@./webdata/$line" "http://127.0.0.1:8000/put" &
     i=$((i+1))
-    if [ $((i % 20)) -eq 0 ]; then
+    if [ $((i % 50)) -eq 0 ]; then
         wait
     fi
 done
